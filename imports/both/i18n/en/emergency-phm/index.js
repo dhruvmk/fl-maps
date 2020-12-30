55e7c663f600c96d920e29dbd983282b94ee03f5
Dhruv Kapu <65883953+dhruvmk@users.noreply.github.com>
2020-12-30 16:05:25 -0500

    Create index.js

---
+++ imports/both/i18n/en/emergency-phm/index.js
@@ -0,0 +1,7 @@
+import EmergencyNewEventModal from "./emergency-new-event-modal.json"
+import EmergencyCategories from "./emergency-categories.json"
+
+export default {
+  EmergencyNewEventModal,
+  EmergencyCategories
+};
